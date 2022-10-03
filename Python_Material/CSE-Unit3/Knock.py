95f4eb24a7af0908c9681b86c8f3481b49e76879
NINJADRAGON32 <mhoward0501@gmail.com>
2022-10-03 09:53:31 -0500

    bagan lits commands

---
+++ Python_Material/CSE-Unit3/Knock.py
@@ -0,0 +1,8 @@
+print("knock knock")
+ui=input("")
+if ui in ["whose there","who's there","who is there","whose there","hoos there","hoos there","who dere","who dat"]:
+    print("you")
+    input("")
+    print(" SIRI : searching the web for yahoo.com")
+else:
+    print("no fun")
