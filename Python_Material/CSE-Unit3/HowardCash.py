785f27aa008ff61b435cfd9c55cf30aa9ad61809
NINJADRAGON32 <mhoward0501@gmail.com>
2022-11-18 09:50:32 -0600

    finals workday

--- Python_Material/CSE-Unit3/HowardCash.py
+++ Python_Material/CSE-Unit3/HowardCash.py
@@ -4,17 +4,35 @@ d=0
 n=0
 p=0
 change = []
-# def coinsOwed(amountOwed):
+# Quarters
 while (amountOwed) >= 0.25:
     amountOwed = amountOwed - 0.25
     q=q+1
+    amountOwed = round(amountOwed,2)
 change.append(f"quarters:{q}")
-print(change)
-print(amountOwed)
+
+# Dimes
 while (amountOwed) >= 0.10:
     amountOwed = amountOwed - 0.10
     d=d+1
+    amountOwed = round(amountOwed,2)
 change.append(f"Dimes:{d}")
-    
-print(change)
+
+
+# nickels
+while (amountOwed) >= 0.05:
+    amountOwed = amountOwed - 0.05
+    n=n+1
+    amountOwed = round(amountOwed,2)
+change.append(f"nickels:{n}")
+
+# pennys
+while (amountOwed) >= 0.01:
+    amountOwed = amountOwed - 0.01
+    p=p+1
+    amountOwed = round(amountOwed,2)
+change.append(f"pennys:{p}")
+
+# check values
 print(amountOwed)
+print(change)
--- Python_Material/CSE-Unit3/HowardMario.py
+++ Python_Material/CSE-Unit3/HowardMario.py
@@ -1,6 +1,18 @@
-from tkinter import HORIZONTAL
-#height
-h = input("height: ")
-h 
+height = 0
+height = int(input("desired height= "))
+#r=row
+#c=column
+
+for r in range(1, height + 1):
+    for c in range(1, height+1):
+        if c > (height - r):
+            print("#", end="")
+        else:
+            print(" ",end=" ")
+        
+        if c==height:
+            print(" ","#" *r, end= "")
+    print()
+
 
 
