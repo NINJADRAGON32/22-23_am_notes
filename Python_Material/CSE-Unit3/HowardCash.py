384c3e721e5425ed291796eb5a70cefcdd9ede54
NINJADRAGON32 <mhoward0501@gmail.com>
2022-11-21 09:54:29 -0600

    today we had a work day after our semester exam

--- Python_Material/CSE-Unit3/HowardCash.py
+++ Python_Material/CSE-Unit3/HowardCash.py
@@ -1,38 +1,59 @@
+# amountOwed = (float(input("change owed(in cents): ")))
+# q=0
+# d=0
+# n=0
+# p=0
+# change = []
+# # Quarters
+# while (amountOwed) >= 0.25:
+#     amountOwed = amountOwed - 0.25
+#     q=q+1
+#     amountOwed = round(amountOwed,2)
+# change.append(f"quarters:{q}")
+
+# # Dimes
+# while (amountOwed) >= 0.10:
+#     amountOwed = amountOwed - 0.10
+#     d=d+1
+#     amountOwed = round(amountOwed,2)
+# change.append(f"Dimes:{d}")
+
+
+# # nickels
+# while (amountOwed) >= 0.05:
+#     amountOwed = amountOwed - 0.05
+#     n=n+1
+#     amountOwed = round(amountOwed,2)
+# change.append(f"nickels:{n}")
+
+# # pennys
+# while (amountOwed) >= 0.01:
+#     amountOwed = amountOwed - 0.01
+#     p=p+1
+#     amountOwed = round(amountOwed,2)
+# change.append(f"pennys:{p}")
+
+# # check values
+# print(amountOwed)
+# print(change)
+
 amountOwed = (float(input("change owed(in cents): ")))
 q=0
 d=0
 n=0
 p=0
 change = []
-# Quarters
-while (amountOwed) >= 0.25:
-    amountOwed = amountOwed - 0.25
-    q=q+1
-    amountOwed = round(amountOwed,2)
-change.append(f"quarters:{q}")
-
-# Dimes
-while (amountOwed) >= 0.10:
-    amountOwed = amountOwed - 0.10
-    d=d+1
-    amountOwed = round(amountOwed,2)
-change.append(f"Dimes:{d}")
-
-
-# nickels
-while (amountOwed) >= 0.05:
-    amountOwed = amountOwed - 0.05
-    n=n+1
-    amountOwed = round(amountOwed,2)
-change.append(f"nickels:{n}")
-
-# pennys
-while (amountOwed) >= 0.01:
-    amountOwed = amountOwed - 0.01
-    p=p+1
-    amountOwed = round(amountOwed,2)
-change.append(f"pennys:{p}")
-
-# check values
+for quarters in range(float(amountOwed)):
+    if quarters%0.25 ==  0:
+        q=q+1
+amountOwed=amountOwed-(q*0.25)
+amountOwed = round(amountOwed,2)
+print(f"quarters=",{q})
+    
+for dimes in range(amountOwed):
+    if dimes%0.10==0:
+        d=d+1
+amountOwed=amountOwed-(d*0.10)
+amountOwed = round(amountOwed,2)
+print(f"dimes=",{d})
 print(amountOwed)
-print(change)
