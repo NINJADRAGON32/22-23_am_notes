f15ba8d09666f00bc873c8468a927d001e2d3872
NINJADRAGON32 <mhoward0501@gmail.com>
2022-12-02 09:57:19 -0600

    today was finals friday

--- Python_Material/TMNT/Section1/WinterPostCard.py
+++ Python_Material/TMNT/Section1/WinterPostCard.py
@@ -8,10 +8,94 @@ wn.bgcolor('black')
 
 #def tree
 
+def snowMan():
+     man =t.Turtle()
+     man.speed(0)
+     man.color('white')
+     man.penup()
+     man.goto(-850,-375)
+     man.pendown()
+     man.fillcolor('white')
+     man.begin_fill()
+     man.circle(50)
+     man.end_fill() 
+     man.begin_fill()
+     man.left(90)
+     man.forward(75)
+     man.right(90)
+     man.circle(40)
+     man.end_fill() 
+     man.left(90)
+     man.forward(65)
+     man.right(90)
+     man.begin_fill()
+     man.circle(30)
+     man.end_fill() 
+     #eyes
+     man.color('black')
+     man.penup()
+     
+
+
+def window():
+     window = t.Turtle()
+     window.color('white')
+     window.penup()
+     window.speed(0)
+     window.goto(-400,-250)
+     window.pendown()
+     window.fillcolor('white')
+     window.begin_fill()
+     for i in range(4):
+          window.forward(800)
+          window.left(90)
+     window.end_fill()
+     window.forward(50)
+     window.left(90)
+     window.forward(50)
+     window.right(90)
+     window.color('black')
+     window.fillcolor('tan')
+     window.begin_fill()
+     for i in range(4):
+          window.forward(700)
+          window.left(90)
+     window.end_fill()
+     window.forward(350)
+     window.color('white')
+     window.pensize(5)
+     window.left(90)
+     window.forward(700)
+     window.left(180)
+     window.forward(350)
+     window.pensize(10)
+     window.left(90)
+     window.forward(350)
+     window.left(180)
+     window.forward(700)
+     window.hideturtle()
+
+
+def ground(numberOfBushes):
+     ground = t.Turtle()
+     ground.speed(0)
+     ground.fillcolor('green')
+     ground.penup()
+     ground.goto(-1000,-350)
+     ground.pendown
+     for i in range (numberOfBushes):
+          ground.begin_fill()
+          for i in range (4):
+               ground.forward(450)
+               ground.right(90)
+          ground.end_fill()
+          ground.forward(500) 
+            
+     
 def presents(numberOfPresents,colorOfPresents):
     
     for i in range(numberOfPresents):
-        p=t.turtle()
+        p=t.Turtle()
         p.color(colorOfPresents)
         p.speed(0)
         p.penup()
@@ -43,7 +127,14 @@ def snow(numberOfFlakes,colorOfSnow,farLeft,farRight,upper,lower,speed,variation
 
 
 #calling the functions
-snow(500,'white',-(wn.window_width()),(wn.window_width()),600,-300,30,5,300)
+
+window()
+presents(1,'red')
+ground(4)
+snowMan()
+snow(300,'white',-1000,1000,600,250,100,5,r.randint(325,350))
+
+
 
 
 
