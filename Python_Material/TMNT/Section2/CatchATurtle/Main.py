277ecb01aa2c735a2398f72d37db46399b5d2092
NINJADRAGON32 <mhoward0501@gmail.com>
2023-01-10 09:57:48 -0600

    cathc a turtle

--- Python_Material/TMNT/Section2/CatchATurtle/Main.py
+++ Python_Material/TMNT/Section2/CatchATurtle/Main.py
@@ -1,13 +1,15 @@
 #---- import 
 import turtle as t
 import random as r
+import leaderboard as lb 
 
 #---- Global Variables
 wn=t.Screen()     
-timer=10
+timer=5
 fontSetup=("Comic Sans",30,"normal")
 interval=1000
 score=0
+FILENAME="Database.txt"
 
 #---- initialize turtles
 bob = t.Turtle()
@@ -30,6 +32,7 @@ scoreKeeper.hideturtle()
 scoreKeeper.goto(100,300)
 scoreKeeper.pd()
 scoreKeeper.speed(0)
+
 #---- functions
 def bobClicked(x,y):
     print("bob clicked")
@@ -45,12 +48,14 @@ def moveBob():
     newX=r.randint(-300,300)
     newY=r.randint(-300,300)
     bob.goto(newX,newY)
+    
 def updatetimer():
     global timer
     timeKeeper.clear()
     if timer<=0:
         timeKeeper.write("Times Up!",font=fontSetup)
         bob.hideturtle()
+        manageLeaderBoard()
     else:
         timer-=1
         timeKeeper.write(f"Timer: {timer}",font=fontSetup)
@@ -62,8 +67,23 @@ def updateScore(x,y):
     scoreKeeper.clear()
     scoreKeeper.write(f"Score: {score}",font=fontSetup)
 
-
-
+def manageLeaderBoard():
+    global score
+    #get datat from text file
+    namesList=lb.getNames(FILENAME)
+    scoresList=lb.getScores(FILENAME)
+    
+    #check to see if you made the laderboard
+    
+    #check to update leaderBoard
+    if(int(len(scoresList))<5 or int(score>=scoresList[-1])):
+        name=input("congrats ur on the board name pls \n\t: ")
+        lb.updateLeaderBoard(FILENAME,namesList,scoresList,name,15)
+    else:
+        print("did not make leaderboard")
+    #display leaderBoard
+    lb.draw_leaderboard(False, namesList, scoresList, scoreKeeper, 10)
+    
 #---- events
 bob.onclick(bobClicked)
 wn.ontimer(updatetimer,interval)
