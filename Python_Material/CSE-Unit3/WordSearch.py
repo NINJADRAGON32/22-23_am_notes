7ee46321c0d3ece9736a3ef45d9e8a7afb395180
NINJADRAGON32 <mhoward0501@gmail.com>
2023-02-10 09:55:26 -0600

    chess

---
+++ Python_Material/CSE-Unit3/WordSearch.py
@@ -0,0 +1,10 @@
+rows=input("how many rows are there:") 
+columns=imput("how many columns are there: ")
+while solved=False:
+    for row in range (rows):
+        for col in range (columns):
+            if ({col}{row})== R:
+                if ({col}+1)({row}) == E or ({})
+            else:
+                pass
+    
